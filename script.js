dd11a290293991852d394aa17d1eec9d1a44e483
Laura Rodriguez <laurarodriguezsimanca@gmail.com>
2023-07-20 15:31:25 -0400

    ramdon color

---
+++ script.js
@@ -0,0 +1,32 @@
+const colorCode = document.querySelector(".color_number");
+const btnGenerate = document.querySelector(".btn_generate");
+const btnCopy = document.querySelector(".btn_copy");
+
+
+function generateColor() {
+
+    let digits = "0123456789ABCDEF";
+    let newColor = "#";
+    
+    for (let i = 0; i < 6; i++) {
+        
+        let random = Math.floor(Math.random() * 16);
+        newColor += digits[random];
+    }
+
+    return newColor;
+}
+
+btnGenerate.addEventListener("click", () => {
+    
+    colorCode.textContent = generateColor();
+    document.body.style.backgroundColor = colorCode.textContent
+})
+
+
+btnCopy.addEventListener("click", () => {
+    
+    console.log(navigator.clipboard.writeText(colorCode.textContent));
+    alert("Copy!")
+})
+
